b79358edfe260a986fc6f64d621cad491172a06f
JosefKru <josefkaru@gmail.com>
2022-10-22 22:05:33 +0300

    my first commit

--- components/BuyMeCoffee/index.jsx
+++ components/BuyMeCoffee/index.jsx
@@ -6,7 +6,7 @@ import ScreenEgg from '../ScreenEgg'
 const BuyMeCoffee = ({ className }) => {
   return (
     <ScreenEgg type="right">
-      <div className={cl(styles.buyCoffee, className)}>
+      <div className={cl(className, styles.buyCoffee)}>
         <a href="#" className={styles.buyCoffeeButton}>
           Send me a message
         </a>
