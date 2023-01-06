eb9bbe5fc11c5cf31c46cd34f4dd9717fece1aa2
JosefKru <josefkaru@gmail.com>
2023-01-07 00:02:26 +0300

    added loading progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/global.scss'
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress
+        color='#73d0a2'
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
